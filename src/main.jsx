4fbfb9cd677069310b0c4a7022d9e1990878b5fe
one-of-its-kind <achalpratapsingh1220@gmail.com>
2024-05-21 11:10:57 +0530

    rm cors

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import cors from "cors";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
@@ -7,12 +6,6 @@ import { store } from "./store";
 import { Provider } from "react-redux";
 import "react-toastify/dist/ReactToastify.css";
 
-const corsOptions = {
-  origin: "https://one-district-one-product-blush.vercel.app/",
-  optionsSuccessStatus: 200,
-};
-
-app.use(cors(corsOptions));
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <Provider store={store}>
